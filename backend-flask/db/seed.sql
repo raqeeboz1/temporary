166ea817e85f5de33ca4d4a375c7387c4d130508
Mohammed Al-Haijamy <mohd.abduraqeeb@gmail.com>
2023-05-07 18:49:25 +0000

    updated seed and schema-load files

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Mohammed Alhaijamy','mohammed@alhaijmay.com','mohammed' ,'MOCK'),
+  ('Ahmed Alhaijamy', 'ahmed@alhaijamy.com','ahmed' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
