0526d9029cd37520d57f0b779ed6c6e6431f7287
Mohammed Al-Haijamy <mohd.abduraqeeb@gmail.com>
2023-05-19 09:13:11 +0000

    dynamo implementation part 1

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,6 +3,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Mohammed Alhaijamy','mohammed@alhaijmay.com','mohammed' ,'MOCK'),
   ('Ahmed Alhaijamy', 'ahmed@alhaijamy.com','ahmed' ,'MOCK');
+  ('Harry Potter', 'harry@alhaijamy.com','harry' ,'MOCK')
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -45,6 +45,7 @@ class Ddb:
         'created_at': last_sent_at
       })
     return results
+
   def list_messages(client,message_group_uuid):
     year = str(datetime.now().year)
     table_name = 'cruddur-messages'
@@ -103,6 +104,7 @@ class Ddb:
       'message': message,
       'created_at': created_at
     }
+    
   def create_message_group(client, message,my_user_uuid, my_user_display_name, my_user_handle, other_user_uuid, other_user_display_name, other_user_handle):
     print('== create_message_group.1')
     table_name = 'cruddur-messages'
