b6e71ca8dc526497588aef7db204d74d023c03db
Mohammed Al-Haijamy <mohd.abduraqeeb@gmail.com>
2023-05-21 10:15:46 +0000

    insert uuid in the seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Mohammed Alhaijamy','mohammed@alhaijmay.com','alhaijamy' ,'MOCK'),
-  ('Ahmed Alhaijamy', 'ahmed@alhaijamy.com','ahmed' ,'MOCK'),
+  ('Mohammed Alhaijamy','mohd.abduraqeeb@gmail.com','alhaijamy' ,'61b60a42-e046-4c56-b231-fcfa0acb581c'),
+  ('Ahmed Alhaijamy', 'mohd.abduraqeeb@gmail.com','ahmed' ,'698ec9c9-7131-4de0-9a95-67cc84bc2ddb'),
   ('Harry Potter', 'harry@alhaijamy.com','harry' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
